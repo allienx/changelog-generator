a3d698faa4fcaf5fd45faf29cf31c9e5001a6f3b
Alex Lin <me@allienx.com>
2026-02-11 21:25:41 -0800

    Go back to cjs

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,11 +1,10 @@
-import * as esbuild from 'esbuild'
+const esbuild = require('esbuild')
 
 esbuild
   .build({
     entryPoints: ['src/main.ts'],
     bundle: true,
     platform: 'node',
-    format: 'esm',
     outfile: 'dist/index.js',
     minify: true,
     sourcemap: true,
