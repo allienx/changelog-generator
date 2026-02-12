4602bc09d6a47b5613720a6b273c2469fa33c937
Alex Lin <allienx@hashmail.cc>
2026-02-11 21:09:25 -0800

    Add fallback to empty tree ref for base

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import * as github from '@actions/github'
 import { compareCommits } from 'src/compare-commits'
 import { getCommitsSummary } from 'src/get-commits-summary'
 
+const GIT_EMPTY_TREE_SHA = '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
+
 async function run() {
   try {
     const token = core.getInput('github-token', { required: true })
-    const base = core.getInput('base', { required: true })
+    const base = core.getInput('base') || GIT_EMPTY_TREE_SHA
     const head = core.getInput('head') || 'HEAD'
     const { owner, repo } = github.context.repo
 
